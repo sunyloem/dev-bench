db4def3493c92e854105b0e2ca67e82a0d035179
xizhen sun <suny.loem@gmail.com>
2025-10-24 14:48:22 -0700

    Remove assets_creator project

--- assets_creator/bin/mcp-assets-creator.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { main } from "../src/main.js";
-
-main(process.argv).catch((error) => {
-  const message = error?.stack ?? error?.message ?? String(error);
-  process.stderr.write(`${message}\n`);
-  process.exit(1);
-});
